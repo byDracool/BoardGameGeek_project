84574ec700aff3d146ceb324ca9c31e1c4095c15
Carlos <carlosterrezsegura@gmail.com>
2025-01-17 10:53:51 +0100

    Styling v6

--- BGG_project/components/footer.py
+++ BGG_project/components/footer.py
@@ -5,6 +5,6 @@ from BGG_project.components.reflex_logo import reflex_logo
 def footer() -> rx.Component:
     return rx.hstack(
                 reflex_logo(),
-                position= "sticky",
+                #position= "sticky",
                 align="end"
     )
--- BGG_project/components/navbar.py
+++ BGG_project/components/navbar.py
@@ -6,7 +6,7 @@ def navbar() -> rx.Component:
                 rx.image(src="/face-logo.png", width="200px", height="auto", align="left", justify="center"),
                 #ESTO HAY QUE CAMBIARLO POR EL LOGIN
                 rx.text(
-                    "Aqui ira login",
+                    "Aquí irá login",
                     align="right",
                     justify="center",
                     size="4",               
