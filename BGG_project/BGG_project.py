783bf121647e75fb136ede012f6307bd94bbdaf1
Carlos <carlosterrezsegura@gmail.com>
2025-01-20 16:50:23 +0100

    Login v4

--- BGG_project/BGG_project.py
+++ BGG_project/BGG_project.py
@@ -1,30 +1,7 @@
 import reflex as rx
-from BGG_project.components.navbar import navbar
-from BGG_project.components.footer import footer
-from BGG_project.views.header.header import header
-from BGG_project.views.finder.finder import finder
-from BGG_project.styles.styles import Size as Size
+from BGG_project.pages.index import index
+from BGG_project.pages.login import login
 import BGG_project.styles.styles as styles
-
-
-class State(rx.State):
-    pass
-
-
-def index() -> rx.Component:  
-    return rx.box(
-        navbar(),
-        rx.center(
-            rx.vstack(                
-                    header(),
-                    finder(),
-                    align="center",
-                    margin_y=Size.DEFAULT.value
-            )
-        ),
-        footer(),
-        #margin_y=Size.SMALL.value
-    )
 
 
 app = rx.App(
@@ -33,6 +10,7 @@ app = rx.App(
     style=styles.BASE_STYLE,
 )
 
-app.add_page(index)
+#app.add_page(index)
+app.add_page(login)
 
 
